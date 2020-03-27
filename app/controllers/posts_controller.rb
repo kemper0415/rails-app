cea582a50840cd7656d4999580d6d2e1e8ae508b
John Doe <kemper04152gmail.com>
2020-03-27 09:01:46 -0400

    init commit rails-app

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+
+  # CRUD = create, read, update delete
+
+# view
+  def index
+  end
+# view
+  def show
+  end
+# view
+  def new
+  end
+
+  def create
+  end
+# view
+  def edit
+  end
+# view
+  def update
+  end
+
+  def delete
+  end
+
+end
